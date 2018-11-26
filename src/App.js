e714b8b77fe4295e4391b6be86b4f3e9b6116a6a
sjlee3157 <sjlee3157@gmail.com>
2018-11-26 14:32:39 -0800

    Wave 1 TimelineEvent component uses Timestamp component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
-import timelineData from './data/timeline.json';
-
+import TimelineEvent from './components/TimelineEvent';
 import Timeline from './components/Timeline';
+
+import timelineData from './data/timeline.json';
+import './App.css';
 
 class App extends Component {
   render() {
@@ -15,6 +16,18 @@ class App extends Component {
           <h1 className="App-title">Application title</h1>
         </header>
         <main className="App-main">
+        <TimelineEvent
+          person="Friekta"
+          status="sad :("
+          eventStamp={new Date("26 Nov 2018 14:28")}
+          key="Friekta1"
+        />
+        <TimelineEvent
+          person="Muzzy"
+          status="aeiou je t'aime <3"
+          eventStamp="2018-11-26T14:25:01"
+          key="Muzzy1"
+        />
         </main>
       </div>
     );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  // Fill in your code here
-  return;
+const TimelineEvent = (props) => {
+  console.log(props);
+  return (
+    <section>
+      <h3>{props.person}</h3>
+      <p>{props.status}</p>
+      <p><Timestamp
+          time={props.eventStamp}
+         />
+      </p>
+    </section>
+  );
 }
 
 export default TimelineEvent;
